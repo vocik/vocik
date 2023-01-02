de8ce0a43d42801b202e2252826c25818771c7d9
vocik <111061384+vocik@users.noreply.github.com>
2023-01-02 14:43:21 +0100

    Add files via upload

---
+++ new 14.js
@@ -0,0 +1,13 @@
+pole[index_pole].hodnota_objekt
+
+var pole=[]
+var objekt={};
+pole.push(objekt);
+
+document.getElementById("utrata").innerHTML+=pocet_polozek + ". " +pole[i].nazev + "<br>";
+
+div
+{
+  float:left;
+  margin: 50px;
+}
